2b25a0a10561d6222ef21ad6f6f1169a16c0ecc4
Arin Rahman <98891277+arinrahman@users.noreply.github.com>
2022-12-21 01:43:58 +0100

    third commit

---
+++ backend/models/post.js
@@ -0,0 +1,12 @@
+const mongoose =require('mongoose');
+
+const postSchema =mongoose.Schema({
+  title:{type: String, required: true},
+  content:{type: String, required: true},
+  startDate:{type: Date, required: true},
+  selectedValue:{type: String, required: true},
+  price:{type: Number, required: true},
+  desc:{type: String},
+  selectedOrigin:{type: String, required: true},
+  favoriteSeason:{type: String, required: true},
+});
