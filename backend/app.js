edbe2d65bfb1f49f8c13fcdf43a8f3773493426d
Arin Rahman <98891277+arinrahman@users.noreply.github.com>
2022-12-21 12:28:57 +0100

    removed dummy data as from now on can fetch real data

--- backend/app.js
+++ backend/app.js
@@ -49,30 +49,7 @@ app.post("/api/posts", (req,res,next)=>
 });
 
 app.get('/api/posts',(req,res,next)=>
-{ const posts=[
-  {id:"1",
-  title:"product name 1",
-  content: "product code 1",
-  startDate: "date 1",
-  selectedValue:"value 1",
-  price: 1,
-  desc: "product description 1",
-  selectedOrigin:"origin 1",
-  favoriteSeason: "is best achieved 1"
-
-},
-{id:"2",
-title:"product name 2",
-content: "product code 2",
-startDate: "date 2",
-selectedValue:"value 2",
-price: 2,
-desc: "product description 2",
-selectedOrigin:"origin 2",
-favoriteSeason: "is best achieved 2"
-
-}
-];
+{ 
   res.status(200).json(
     {
       message: "post fetched success",
