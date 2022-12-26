c3b53180292726b8690589b2797be29928f3d821
Arin Rahman <98891277+arinrahman@users.noreply.github.com>
2022-12-27 00:57:56 +0100

    adding validation

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,16 +1,53 @@
 const mongoose =require('mongoose');
 
 const postSchema =mongoose.Schema({
-  title:{type: String, required: true},
-  content:{type: String, required: true},
-  startDate:{type: Date, required: true},
-  selectedValue:{type: String, required: true},
-  price:{type: Number, required: true},
-  desc:{type: String},
-  selectedOrigin:{type: String, required: true},
-  favoriteSeason:{type: String, required: true},
-  imageURL:{type: String, required: true},
-  quantity:{type: Number, required: true}
+  title:{
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  content:{
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  startDate:{
+    type: Date,
+    required: true
+
+    },
+  selectedValue:{
+    type: String,
+    required: true
+  },
+  price:{
+    type: Number,
+    required: true
+  },
+  desc:{
+    type: String,
+    minlength: 3,
+    maxlength: 250,
+    required: false
+  },
+  selectedOrigin:{
+    type: String,
+    required: true
+  },
+  favoriteSeason:{
+    type: String,
+    required: false
+  },
+  imageURL:{
+    type: String,
+    required: false
+  },
+  quantity:{
+    type: Number,
+    required: false
+  }
 });
 
 module.exports= mongoose.model('Post', postSchema);
