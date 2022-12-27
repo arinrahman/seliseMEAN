2cb95785301a88d6c3c00e9eae2d8b40f813c346
Arin Rahman <98891277+arinrahman@users.noreply.github.com>
2022-12-27 04:02:50 +0100

    changed form to reactive form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit} from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { FormControl, FormGroup, Validators} from "@angular/forms";
 import { ActivatedRoute, ParamMap } from "@angular/router";
 import { Post } from "../post.module";
 import { PostsService } from "../posts.service";
@@ -7,45 +7,70 @@ interface Food {
   value: string;
   viewValue: string;
 }
-
-interface Place {
+interface Origin {
   value: string;
   viewValue: string;
 }
-
+interface Fav {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-
 export class PostCreateComponent implements OnInit{
-  labelPosition: 'before' | 'after' = 'after';
-  startDate = new Date();
-  selectedValue: string;
-  selectedOrigin:string;
-  favoriteSeason: string;
+  foods: Food[] = [
+    {value: 'Makeup', viewValue: 'Makeup'},
+    {value: 'Skincare', viewValue: 'Skincare'},
+    {value: 'Baby Products', viewValue: 'BabyProducts'},
+  ];
+
+  origins: Origin[] = [
+    {value: 'Bangladesh', viewValue: 'Bangladesh'},
+    {value: 'USA', viewValue: 'USA'},
+    {value: 'Korea', viewValue: 'Korea'},
+  ];
+  favs: Fav[] = [
+    {value: 'Day', viewValue: 'Day'},
+    {value: 'Night', viewValue: 'Night'},
+    {value: 'Day/Night', viewValue: 'Day/Night'},
+  ];
   private mode='post-create';
   private postId:string;
   post: Post;
   isLoading= false;
-
-  seasons: string[] = ['Day', 'Night'];
-
-  foods: Food[] = [
-    {value: 'makeup', viewValue: 'Makeup'},
-    {value: 'skincare', viewValue: 'Skin care'},
-
-  ];
-  places: Place[] = [
-    {value: 'Bangladesh', viewValue: 'Bangladesh'},
-    {value: 'USA', viewValue: 'USA'},
-
-  ];
+  form:FormGroup;
 constructor(public postsService: PostsService, public route: ActivatedRoute){
-
 }
 ngOnInit(){
+  this.form= new FormGroup(
+    {
+      'title': new FormControl(null, {validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
+      }),
+      'content': new FormControl(null, {validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
+      }),
+      'selectedValue': new FormControl(null, {validators: [Validators.required]
+      }),
+      'price': new FormControl(null, {validators: [Validators.required, Validators.min(0)]
+      }),
+      'desc': new FormControl(null,{validators: [Validators.minLength(3), Validators.maxLength(250)]
+      }
+      ),
+      'favoriteSeason': new FormControl(null, {validators: [Validators.required]
+      }),
+      'startDate': new FormControl(null, {validators: [Validators.required]
+      }),
+      'selectedOrigin': new FormControl(null, {validators: [Validators.required]
+      }),
+      'imageURL': new FormControl(null
+      ),
+      'quantity': new FormControl(null, {validators: [Validators.min(0)]
+      }),
+    }
+
+);
   this.route.paramMap.subscribe((paramMap: ParamMap)=>{
     if(paramMap.has('postId')){
       this.mode='edit';
@@ -67,7 +92,20 @@ ngOnInit(){
       quantity: postData.quantity
 
     };
-   }     );
+    this.form.setValue({
+      'title': this.post.title,
+      'content': this.post.content,
+      'selectedOrigin': this.post.selectedOrigin,
+     ' selectedValue':this.post.selectedValue,
+     ' price': this.post.price,
+     'desc': this.post.desc,
+      'startDate': this.post.startDate,
+      'favoriteSeason': this.post.favoriteSeason,
+     ' imageURL': this.post.imageURL,
+      'quantity': this.post.quantity
+    });
+
+      });
     }else{
       this.mode = 'post-create';
       this.postId = null;
@@ -75,22 +113,44 @@ ngOnInit(){
   });
 
 }
-  onSavePost(form: NgForm)
+  onSavePost()
   {
-    if(form.invalid){
+    if(this.form.invalid){
       return;
     }
-    
+
      if(this.mode==="post-create")
     {
-      this.postsService.addPost(form.value.title,form.value.content, form.value.startDate, form.value.selectedValue, form.value.price, form.value.desc, form.value.selectedOrigin, form.value.favoriteSeason, form.value.imageURL, form.value.quantity);
+      this.postsService.addPost(
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.startDate,
+        this.form.value.selectedValue,
+        this.form.value.price,
+        this.form.value.desc,
+        this.form.value.selectedOrigin,
+        this.form.value.favoriteSeason,
+        this.form.value.imageURL,
+        this.form.value.quantity
+        );
       alert("You succesfully added a product!")
 
     }
     else{
-      this.postsService.updatePost(this.postId, form.value.title,form.value.content, form.value.startDate, form.value.selectedValue, form.value.price, form.value.desc, form.value.selectedOrigin, form.value.favoriteSeason, form.value.imageURL, form.value.quantity);
+      this.postsService.updatePost(this.postId,
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.startDate,
+        this.form.value.selectedValue,
+        this.form.value.price,
+        this.form.value.desc,
+        this.form.value.selectedOrigin,
+        this.form.value.favoriteSeason,
+        this.form.value.imageURL,
+        this.form.value.quantity);
       alert("You succesfully editted a product!")
     }
+    this.form.reset();
 
   }
 }
